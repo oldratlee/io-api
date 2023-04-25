1f0546c043025b430b2e89ccc2e27e386d17127e
Jerry Lee <oldratlee@gmail.com>
2023-04-25 15:10:47 +0800

    test: add a dummy unit test

---
+++ src/test/java/com/oldratlee/io/IoApiTest.java
@@ -0,0 +1,11 @@
+package com.oldratlee.io;
+
+import com.oldratlee.io.demo.Demo_FileTransport;
+import org.junit.jupiter.api.Test;
+
+class IoApiTest {
+    @Test
+    void helloWorld() throws Exception {
+        Demo_FileTransport.main(new String[0]);
+    }
+}
